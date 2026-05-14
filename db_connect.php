3b3f28d27a674333f41fb870f60e296ef686112a
codecodecode <vuquockhai88@gmail.com>
2026-05-14 21:49:08 +0700

    update port

--- db_connect.php
+++ db_connect.php
@@ -3,7 +3,7 @@ $servername = "127.0.0.1";
 $username = "root";
 $password = ""; // Nếu XAMPP của Khải không đặt pass thì để trống
 $dbname = "cakery"; // Tên database thật vừa nhập vào
-$port = 3307; // Cổng của XAMPP Khải đang dùng
+$port = 3306; // Cổng của XAMPP Khải đang dùng
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
